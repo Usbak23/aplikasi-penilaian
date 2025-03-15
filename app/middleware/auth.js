db66fa8dcd6aa9bc07d2ef6b4c03a28ff107de73
Usbak23 <amubarok1998@gmail.comm>
2025-03-15 21:23:11 +0700

    deploy

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,30 +1,20 @@
 
 
-// module.exports = {
-    
-//     isLoginAdmin: (req, res, next) => {
-//         console.log("DEBUG SESSION USER:", req.session.user);
-//         if (
-//             req.session.user === null ||
-//             req.session.user === undefined ||
-//             req.session.user.role !== "administrator"
-//         ) {
-//             req.flash(
-//                 "alertMessage",
-//                 `Mohon Maaf Sesi Anda Telah Habis, Silahkan Login Kembali`
-//             );
-//             req.flash("alertStatus", "danger");
-//             res.redirect("/");
-//         } else {
-//             next();
-//         }
-// }
-// }
+
 
 module.exports = {
-    isLoginAdmin: (req, res, next) => {
+    isLoginAdministrator: (req, res, next) => {
         console.log("DEBUG SESSION USER:", req.session.user);
         if (req.session.user === null || req.session.user === undefined || req.session.user.role !== "administrator") {
+            req.flash("alertMessage", `Mohon Maaf Sesi Anda Telah Habis, Silahkan Login Kembali`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/");
+        } else{
+            next()
+        }
+    },
+    isLoginAdmin: (req, res, next) => {
+        if (req.session.user === null || req.session.user === undefined || req.session.user.role !== "admin") {
             req.flash("alertMessage", `Mohon Maaf Sesi Anda Telah Habis, Silahkan Login Kembali`);
             req.flash("alertStatus", "danger");
             res.redirect("/");
@@ -43,60 +33,3 @@ module.exports = {
         }
     }
 }
-// }
-//  module.exports = {
-//     // Middleware untuk semua user yang login
-//     isLoginAdmin: (req, res, next) => {
-//         if (req.session.user === null || req.session.user === undefined) {
-//             req.flash("alertMessage", `Mohon Maaf Sesi Anda Telah Habis, Silahkan Login Kembali`);
-//             req.flash("alertStatus", "danger");
-//             res.redirect("/");
-//         } else{
-//             next()
-//         }
-//     },
-
-//     // Middleware untuk role admin dan administrator
-//     isRoleAdminOrAdministrator: (req, res, next) => {
-//         if (!req.session.user) {
-//             req.flash("alertMessage", `Mohon Maaf Sesi Anda Telah Habis, Silahkan Login Kembali`);
-//             req.flash("alertStatus", "danger");
-//             return res.redirect("/");
-//         }
-
-//         if (req.session.user.role !== "admin" && req.session.user.role !== "administrator") {
-//             req.flash("alertMessage", `Akses Ditolak. Halaman ini hanya dapat diakses oleh Admin atau Administrator`);
-//             req.flash("alertStatus", "danger");
-//             return res.redirect("/"); // Redirect ke dashboard jika role tidak sesuai
-//         }
-
-//         next(); // Lanjutkan ke route berikutnya jika role sesuai
-//     },
-
-//     // Middleware untuk role administrator saja
-//     isLoginAdministrator: (req, res, next) => {
-//         if (!req.session.user) {
-//             req.flash("alertMessage", `Mohon Maaf Sesi Anda Telah Habis, Silahkan Login Kembali`);
-//             req.flash("alertStatus", "danger");
-//             return res.redirect("/");
-//         }
-
-//         if (req.session.user.role !== "administrator") {
-//             req.flash("alertMessage", `Halaman ini hanya dapat diakses oleh Administrator`);
-//             req.flash("alertStatus", "danger");
-//             return res.redirect("/");
-//         }
-
-//         next();
-//     },
-
-//     isPeserta: (req, res, next) => {
-//         if (req.session && req.session.user && req.session.user.role === "peserta") {
-//             return next();
-//           } else {
-//             req.flash("alertMessage", "Anda harus login sebagai peserta untuk mengakses fitur ini.");
-//             req.flash("alertStatus", "danger");
-//             return res.redirect("/login");
-//           }
-// }
-// }
