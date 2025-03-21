70d0222b05e61718fc5ea963f32854dd33b04b4a
Usbak23 <amubarok1998@gmail.comm>
2025-03-22 00:14:23 +0700

    chore: panambahan fitur midtest

---
+++ app/midTest/model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+let midTestSchema = mongoose.Schema(
+    {
+        namePemandu: {
+            type: mongoose.Schema.Types.String,
+            ref: "User",
+            required: [true, "Admin tidak boleh kosong"],
+        },
+       
+        namePeserta:[ {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Peserta",
+            required: [true, "Peserta tidak boleh kosong"],
+        }],
+        nilaiMidtest:{
+            type: Number,
+            required: [true, "Nilai Middle Test harus diisi"],
+            min: [50, "Nilai Persepsi tidak boleh kurang dari 50"],
+            max: [80, "Nilai Persepsi tidak boleh lebih dari 80"],
+        },
+    },
+    { timestamps: true }
+);  
+
+module.exports = mongoose.model("midTest", midTestSchema);
